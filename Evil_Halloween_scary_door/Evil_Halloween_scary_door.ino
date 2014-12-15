ea903ce9ff0c0579de907f6e98918efed787fd23
catchcoder <catchcoder@gmail.com>
2014-12-15 15:58:39 +0000

    Updated code and chnage extension to in

--- Evil_Halloween_scary_door/Evil_Halloween_scary_door.ino
+++ Evil_Halloween_scary_door/Evil_Halloween_scary_door.ino
@@ -38,130 +38,6 @@ byte lastSample;
 const int buttonPin = 2;     // the number of the pushbutton pin
 int buttonState = 0; 
 int buttonDebounce = 0;
-
-// This is called at 8000 Hz to load the next sample.
-ISR(TIMER1_COMPA_vect) {
-    if (sample >= sounddata_length) {
-        if (sample == sounddata_length + lastSample) {
-            stopPlayback();
-        }
-        else {
-            // Ramp down to zero to reduce the click at the end of playback.
-            OCR2A = sounddata_length + lastSample - sample;
-        }
-    }
-    else {
-        OCR2A = pgm_read_byte(&sounddata_data[sample]);
-    }
-
-    ++sample;
-}
-
-void startPlayback()
-{
-    pinMode(speakerPin, OUTPUT);
-
-    // Set up Timer 2 to do pulse width modulation on the speaker
-    // pin.
-
-    // Use internal clock (datasheet p.160)
-    ASSR &= ~(_BV(EXCLK) | _BV(AS2));
-
-    // Set fast PWM mode  (p.157)
-    TCCR2A |= _BV(WGM21) | _BV(WGM20);
-    TCCR2B &= ~_BV(WGM22);
-
-    // Do non-inverting PWM on pin OC2A (p.155)
-    // On the Arduino this is pin 11.
-    TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
-    TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
-
-    // No prescaler (p.158)
-    TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
-
-    // Set initial pulse width to the first sample.
-    OCR2A = pgm_read_byte(&sounddata_data[0]);
-
-
-    // Set up Timer 1 to send a sample every interrupt.
-
-    cli();
-
-    // Set CTC mode (Clear Timer on Compare Match) (p.133)
-    // Have to set OCR1A *after*, otherwise it gets reset to 0!
-    TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
-    TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
-
-    // No prescaler (p.134)
-    TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
-
-    // Set the compare register (OCR1A).
-    // OCR1A is a 16-bit register, so we have to do this with
-    // interrupts disabled to be safe.
-    OCR1A = F_CPU / SAMPLE_RATE;    // 16e6 / 8000 = 2000
-
-    // Enable interrupt when TCNT1 == OCR1A (p.136)
-    TIMSK1 |= _BV(OCIE1A);
-
-    lastSample = pgm_read_byte(&sounddata_data[sounddata_length-1]);
-    sample = 0;
-    sei();
-}
-
-void stopPlayback()
-{
-    // Disable playback per-sample interrupt.
-    TIMSK1 &= ~_BV(OCIE1A);
-
-    // Disable the per-sample timer completely.
-    TCCR1B &= ~_BV(CS10);
-
-    // Disable the PWM timer.
-    TCCR2B &= ~_BV(CS10);
-
-    digitalWrite(speakerPin, LOW);
-}
-
-void setup()
-{
-  //startPlayback();
-    pinMode(ledPin, OUTPUT);
-    
-    pinMode(buttonPin, INPUT);     
-}
-
-void loop()
-{
-  buttonState = digitalRead(buttonPin);
-
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  digitalWrite ( ledPin, buttonState);
-  if (buttonState == HIGH && buttonDebounce == HIGH) {
-    buttonDebounce = LOW ;
-  }
-  
-  if (buttonState == LOW && buttonDebounce == LOW) {     
-    // turn LED on:    
-    buttonDebounce = HIGH;
-    delay(2000);
-    startPlayback(); 
-    startPlayback(); 
-    delay (5000);
-  }
- buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH && buttonDebounce == HIGH) {
-    buttonDebounce = LOW ;
-  }
- //   
-}
-
-
-// sounddata sound made by wav2c
-// (wav2c modified to use unsigned samples)
-
-/* const int sounddata_sampleRate=8000; */
-/*
 const int sounddata_length=10000;
 
 const unsigned char sounddata_data[] PROGMEM = {
@@ -369,4 +245,131 @@ const unsigned char sounddata_data[] PROGMEM = {
 	128,128,129,128,128,127,128,128,129,128,128,128,128,128,128,128,128,129,127,128,128,128,128,129,128,128,128,128,128,128,128,128,128,128,128,128,127,128,128,128,128,128,128,127,127,128,127,128,128,128,128,128,128,128,128,128,129,128,128,128,129,128,129,128,128,128,127,128,128,128,128,128,128,128,127,128,128,128,128,129,128,128,128,129,127,129,128,129,128,127,127,127,128,129,128,128,128,129,128,128,
 	128,128,128,128,128,128,128,
 };
-*/
+
+
+// This is called at 8000 Hz to load the next sample.
+ISR(TIMER1_COMPA_vect) {
+    if (sample >= sounddata_length) {
+        if (sample == sounddata_length + lastSample) {
+            stopPlayback();
+        }
+        else {
+            // Ramp down to zero to reduce the click at the end of playback.
+            OCR2A = sounddata_length + lastSample - sample;
+        }
+    }
+    else {
+        OCR2A = pgm_read_byte(&sounddata_data[sample]);
+    }
+
+    ++sample;
+}
+
+void startPlayback()
+{
+    pinMode(speakerPin, OUTPUT);
+
+    // Set up Timer 2 to do pulse width modulation on the speaker
+    // pin.
+
+    // Use internal clock (datasheet p.160)
+    ASSR &= ~(_BV(EXCLK) | _BV(AS2));
+
+    // Set fast PWM mode  (p.157)
+    TCCR2A |= _BV(WGM21) | _BV(WGM20);
+    TCCR2B &= ~_BV(WGM22);
+
+    // Do non-inverting PWM on pin OC2A (p.155)
+    // On the Arduino this is pin 11.
+    TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
+    TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
+
+    // No prescaler (p.158)
+    TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+
+    // Set initial pulse width to the first sample.
+    OCR2A = pgm_read_byte(&sounddata_data[0]);
+
+
+    // Set up Timer 1 to send a sample every interrupt.
+
+    cli();
+
+    // Set CTC mode (Clear Timer on Compare Match) (p.133)
+    // Have to set OCR1A *after*, otherwise it gets reset to 0!
+    TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
+    TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
+
+    // No prescaler (p.134)
+    TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+
+    // Set the compare register (OCR1A).
+    // OCR1A is a 16-bit register, so we have to do this with
+    // interrupts disabled to be safe.
+    OCR1A = F_CPU / SAMPLE_RATE;    // 16e6 / 8000 = 2000
+
+    // Enable interrupt when TCNT1 == OCR1A (p.136)
+    TIMSK1 |= _BV(OCIE1A);
+
+    lastSample = pgm_read_byte(&sounddata_data[sounddata_length-1]);
+    sample = 0;
+    sei();
+}
+
+void stopPlayback()
+{
+    // Disable playback per-sample interrupt.
+    TIMSK1 &= ~_BV(OCIE1A);
+
+    // Disable the per-sample timer completely.
+    TCCR1B &= ~_BV(CS10);
+
+    // Disable the PWM timer.
+    TCCR2B &= ~_BV(CS10);
+
+    digitalWrite(speakerPin, LOW);
+}
+
+void setup()
+{
+  //startPlayback();
+    pinMode(ledPin, OUTPUT);
+    
+    pinMode(buttonPin, INPUT);     
+}
+
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  digitalWrite ( ledPin, buttonState);
+  if (buttonState == HIGH && buttonDebounce == HIGH) {
+    buttonDebounce = LOW ;
+  }
+  
+  if (buttonState == LOW && buttonDebounce == LOW) {     
+    // turn LED on:    
+    buttonDebounce = HIGH;
+    delay(2000);
+    startPlayback(); 
+    startPlayback(); 
+    delay (5000);
+  }
+ buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH && buttonDebounce == HIGH) {
+    buttonDebounce = LOW ;
+  }
+ //   
+}
+
+
+// sounddata sound made by wav2c
+// (wav2c modified to use unsigned samples)
+
+/* const int sounddata_sampleRate=8000; */
+
+
+
+
